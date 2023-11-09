b3c60c94cc4830776578ae37e7a161de4bc70242
Färber Torben <torben.faerber@baumann-automation.com>
2023-11-09 06:19:17 +0100

    Added first version of the search/starter script

---
+++ sourcetree-open-repository.ps1
@@ -0,0 +1,10 @@
+﻿param (
+      [string]$repository
+)
+
+if ($repository) {
+  $solutionFile = Get-ChildItem -Path $repository -Recurse -Filter *.sln | Select-Object -first 1
+  if ($solutionFile) {
+    Start-Process $solutionFile.FullName
+  }
+}
